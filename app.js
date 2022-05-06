52c771813d3f9cc20bad923a0d4be63597e436f8
Indrė <90854409+IndrePet@users.noreply.github.com>
2022-05-06 15:12:37 +0300

    adding templates & date module

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ import express from 'express';
 import * as url from 'url';
 import bodyParser from 'body-parser';
 
+import { getDate } from './date.js';
+
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 const app = express();
 const port = 3000;
 
+const day = getDate();
+
 const items = ['Buy food', 'Cook food', 'Eat food'];
+const workItems = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -17,22 +22,27 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  const today = new Date();
-  const options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long',
-  };
-
-  const day = today.toLocaleDateString('en-us', options);
-
-  res.render('list', { kindOfDay: day, newListItems: items });
+  res.render('list', { listTitle: day, newListItems: items });
 });
 
 app.post('/', (req, res) => {
   const item = req.body.newItem;
-  items.push(item);
+  if (req.body.list === 'Work') {
+    workItems.push(item);
+    res.redirect('/work');
+  } else {
+    items.push(item);
+  }
+
   res.redirect('/');
+});
+
+app.get('/work', (req, res) => {
+  res.render('list', { listTitle: 'Work', newListItems: workItems });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
 });
 
 app.listen(port, () => {
---
+++ date.js
@@ -0,0 +1,14 @@
+const getDate = () => {
+  const today = new Date();
+  const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+  };
+
+  const day = today.toLocaleDateString('en-us', options);
+
+  return day;
+};
+
+export { getDate };
